b8105b5704b1c8353c76470051bc24c3aa4a5207
harsh kochar <harshkochar88@gmail.com>
2025-07-12 16:31:53 +0530

    #Fixed

--- src/register.js
+++ src/register.js
@@ -14,6 +14,7 @@ const PhoneInput = ({ value, onChange, countryCode, onCountryChange, placeholder
     { code: '+65', country: 'Singapore' }
   ];
 
+  
   return (
     <div className="flex">
       <select
